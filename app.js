a3e94f3766d045e79af961e5550e5760473eb9cf
nishant620 <nishantkumargupta63@gmail.com>
2026-01-30 21:45:20 +0530

    Fix Render session + env config

--- app.js
+++ app.js
@@ -47,18 +47,27 @@ const sessionOptions = {
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
+    sameSite: "none",
   },
 };
+
 
 // app.get("/", (req, res) => {
 //   res.send("Hii,I am root!");
 // });
+<<<<<<< HEAD
 app.get("/", (req, res) => {
   res.redirect("/listings");
 });
+=======
+
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
+>>>>>>> e84db8f (Fix Render session + env config)
 app.use(session(sessionOptions));
 app.use(flash());
 
